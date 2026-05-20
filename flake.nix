16bfb7cc137b04b9bd35a027b4a922a08b95d7e1
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-05-20 17:45:18 -0400

    Commit: cleaned up file and comments, added sops-nix and binary caches, changed nixConfig to nix.settings.

--- flake.nix
+++ flake.nix
@@ -1,37 +1,37 @@
-# ---------------------------------------------------
-# Nixos w/ Flakes, Home Manager, and Multiple Inputs
-# ---------------------------------------------------
+# ------------------------------------------------------------------------
+# Nixos Top-Level Flake w/ Multiple Input Channels & Caches
+# ------------------------------------------------------------------------
 
 {
-  description = "Nick's NixOS Conf with Flakes & Home Manager'";
+  description = "Nick's NixOS Top-Level Flake";
 
   inputs = {
+    # Nixpkgs Stable
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    # Nixpkgs Unstable
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # NumtideLLM Agents
     llm-agents.url = "github:numtide/llm-agents.nix";
+    # SOPS Nix
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Vicinae
     vicinae.url = "github:vicinaehq/vicinae";
   };
 
-    nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://vicinae.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-    ];
-  };
-
-  outputs = { self, nixpkgs, home-manager, nixpkgs-unstable, llm-agents, vicinae, ... }@inputs:
+  # Channel outputs to be used by system modules 
+  outputs = { self, nixpkgs, home-manager, nixpkgs-unstable, llm-agents, sops-nix, vicinae, ... }@inputs:
   
   let 
     system = "x86_64-linux";
-
+    # Define nixpkgs-unstable as pkgsUnstable to be shared with configuration.nix and home.nix
     pkgsUnstable = import nixpkgs-unstable {
       inherit system;
       config = {
@@ -46,19 +46,37 @@
     nixosConfigurations.nixadmin = nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit inputs pkgsUnstable; };
-      
+      # Configuration modules 
       modules = [
-           ./configuration.nix
-           home-manager.nixosModules.home-manager
-           {
-             home-manager.useGlobalPkgs = true;
-             home-manager.useUserPackages = true;
-             home-manager.extraSpecialArgs = { inherit inputs pkgsUnstable; };
-             home-manager.users.nixadmin = import ./home.nix;
-             home-manager.backupFileExtension = "backup";
-           }
-         ];
-       };
-     };
-
+        # Core Configuration
+        ./configuration.nix
+        # Input channel binary caches
+        ({ config, pkgs, ... }: {
+          nix.settings.substituters = [
+            "https://nix-community.cachix.org"
+            "https://cache.nixos.org"
+            "https://cache.numtide.com"
+            "https://vicinae.cachix.org"
+          ];
+          nix.settings.trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "cache.nixos.org-1:mCHdDbf9VMTLI1uSRunKadHYADjL5YhBJWhhSPK6QR0="
+            "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+            "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+          ];
+        })
+        # Home Manager configuration for user
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs pkgsUnstable; };
+          home-manager.users.nixadmin = import ./home.nix;
+          home-manager.backupFileExtension = "backup";
+        }
+        # SOPS configuration for secrets management
+        sops-nix.nixosModules.sops
+      ];
+    };
+  };
 }
