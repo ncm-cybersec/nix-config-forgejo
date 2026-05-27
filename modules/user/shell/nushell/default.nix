27383f533fabba42fe05964210053a72437ad696
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-05-27 14:35:26 -0400

    Commit: modified starship config

--- modules/user/shell/nushell/default.nix
+++ modules/user/shell/nushell/default.nix
@@ -43,13 +43,7 @@
    
    starship = { 
      enable = true;
-     settings = {
-       add_newline = true;
-       character = {
-       success_symbol = "[➜](bold green)";
-         error_symbol = "[➜](bold red)";
-       };
-     };
+     settings = builtins.fromTOML (builtins.readFile ./starship.toml);
    };
  };
 
