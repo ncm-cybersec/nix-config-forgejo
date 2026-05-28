b2662f54c8f3f5973bd6fd734ebf6705eedae975
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-05-28 12:13:18 -0400

    Commit: removed argument

--- modules/system/services/system/default.nix
+++ modules/system/services/system/default.nix
@@ -2,7 +2,7 @@
 # Nixos System Services
 # ---------------------------------------------------
 
-{ config, pkgs, pkgsUnstable, ... }:
+{ pkgs, pkgsUnstable, ... }:
 
 {
   # Enable Avahi/mDNS for network discovery
