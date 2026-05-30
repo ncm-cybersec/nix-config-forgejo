5d1b2ffd3c8a157938642d4056bfb63adcead1ee
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-05-30 10:36:28 -0400

    Commit: added awesome fastfetch config and aliases

--- modules/system/shell/default.nix
+++ modules/system/shell/default.nix
@@ -17,9 +17,11 @@
 
     eval "$(starship init zsh)"
 
-    fastfetch
+    fastfetch -c examples/25 
   '';
 
+  # Thank you to https://github[.]com/sircam-html/nixos-conf for "fastfetch -c examples/25"!
+  
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -27,6 +29,22 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     histSize = 10000;
+    shellAliases = {
+      c = "clear";
+      cdu = "cd ..";
+      et = "eza --tree";
+      ga = "git add .";
+      gb = "git branch -m main";
+      gi = "git init";
+      gm = "git commit -m";
+      gs = "git status";
+      gsp = "git status -s -b";
+      gpm = "git push -u origin main";
+      ipas = "ip addr show";
+      ll = "ls -la";
+      nfu = "cd nix-config && nix flake update";
+      nrs = "sudo nixos-rebuild switch";
+    };
     ohMyZsh = {
       enable = true;
       plugins = [
