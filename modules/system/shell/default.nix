f9b95845b036c0c30e1d964956e4c6d29f2d6662
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-05-30 21:14:50 -0400

    Commit: modified zsh config

--- modules/system/shell/default.nix
+++ modules/system/shell/default.nix
@@ -22,4 +22,40 @@
 
   # Thank you to https://github[.]com/sircam-html/nixos-conf for "fastfetch -c examples/25"!
   
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableBashCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    histSize = 10000;
+    shellAliases = {
+      c = "clear";
+      cdu = "cd ..";
+      et = "eza --tree";
+      ga = "git add .";
+      gb = "git branch -m main";
+      gi = "git init";
+      gm = "git commit -m";
+      gs = "git status";
+      gsp = "git status -s -b";
+      gpm = "git push -u origin main";
+      ipas = "ip addr show";
+      ll = "ls -la";
+      nfu = "cd nix-config && nix flake update";
+      nrs = "sudo nixos-rebuild switch";
+    };
+    ohMyZsh = {
+      enable = true;
+      plugins = [
+        "branch"
+        "git"
+        "ssh-agent"
+        "ssh"
+        "starship"
+        "systemd"
+        "tailscale"
+      ];
+    };
+  };
 }
