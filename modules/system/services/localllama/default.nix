cdc04b54360652068736fb0a1880e6bb94b65b96
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-03 18:36:53 -0400

    system(services): modified config

---
+++ modules/system/services/localllama/default.nix
@@ -0,0 +1,14 @@
+# ---------------------------------------------------
+# Nixos Productivity Services
+# ---------------------------------------------------
+
+{ pkgs, pkgsUnstable, ... }:
+
+{
+  # Enable Ollama for local llms
+  services.ollama = {
+    enable = true;
+    package = pkgs.ollama-cuda;
+  };
+
+}
