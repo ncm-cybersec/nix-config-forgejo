06f333d0699a94fbc2b180b4cc3f5277b7acb442
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-06 09:33:20 -0400

    switched refind back to grub temporarily

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -1,5 +1,7 @@
 # ==========================================================================
-# Bootloader Configuration - rEFInd chainloads systemd-boot
+# Bootloader Configuration - Currently using Grub
+#
+# rEFInd chainloading systemd-boot config is below (commented out)
 # ==========================================================================
 
 { 
@@ -9,21 +11,34 @@
 
 {
 
-  # Bootloader configuration; enabled systemd-boot for generation tracking (rEFInd will chainload this)
+  # Disable systemd-boot
+  boot.loader.systemd-boot.enable = false;
+
+  # Grub bootloader configuration
   boot.loader = {
-    systemd-boot.enable = true;
     efi = {
-      # EFI variables disabled to prevent systemd-boot from taking over rEFInd
-      canTouchEfiVariables = false;
+      canTouchEfiVariables = true;
       efiSysMountPoint = "/boot";
     };
-  };
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      useOSProber = true;
+      splashImage = "/home/nixadmin/nix-config/assets/emperor.png";
 
-  # Add refind package so we can run `refind-install`
-  # See refindtheme.conf for custom theme and complete configuration steps for NixOS.
-  environment.systemPackages = with pkgs; [
-    refind
-  ];
+      # Manual Grub Menu entry for Manjaro on nvme1n1p1
+      extraEntries = ''
+        menuentry "Manjaro Linux" {
+          insmod part_gpt
+          insmod fast
+          insmod chain
+          search --no-floppy --fs-uuid --set=root 6B9A-BB79
+          chainloader /EFI/manjaro/grubx64.efi
+        }
+      '';
+    };
+  };
 
   # Use latest Zen kernel 7.0.10
   boot.kernelPackages = pkgs.linuxPackages_zen;
@@ -46,5 +61,25 @@
 
   # I2C kernel modules for openrgb
   hardware.i2c.enable = true;
+
+  # ==========================================================================
+  # rEFInd Bootloader Configuration - rEFInd chainloads systemd-boot
+  # ==========================================================================
+
+  # Bootloader configuration; enabled systemd-boot for generation tracking (rEFInd will chainload this)
+  #boot.loader = {
+    #systemd-boot.enable = true;
+    #efi = {
+      # EFI variables disabled to prevent systemd-boot from taking over rEFInd
+      #canTouchEfiVariables = false;
+      #efiSysMountPoint = "/boot";
+    #};
+  #};
+
+  # Add refind package so we can run `refind-install`
+  # See refindtheme.conf for custom theme and complete configuration steps for NixOS.
+  #environment.systemPackages = with pkgs; [
+    #refind
+  #];
 
 }
