8b874dd87da5d428da6e13b635aabfd66d9a3ec4
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-07 03:36:12 -0400

    modified vicinae config

--- modules/user/utilities/vicinae/default.nix
+++ modules/user/utilities/vicinae/default.nix
@@ -8,13 +8,36 @@
   ... 
 }:
 
-{
+let
+  # Vicinae theme data is tracked in this repo, with the function below parsing the .toml file into a Nix attrset, while also symlinking to the correct path for Vicinae ~/.config/vicinae/themes/.
+  rawThemeData = builtins.fromTOML (builtins.readFile ./rose-pine-moon.toml);
+
+in {
+  # Links rose-pine-moon.png to the correct path for Vicinae ~/.config/vicinae/themes/icons/.
+  home.file.".config/vicinae/themes/icons/rose-pine-moon.png".source = ./rose-pine.png;
+
+    
   programs.vicinae = {
     enable = true;
     systemd = {
       enable = true;
       autoStart = true;
     };
+    useLayerShell = true;
+    settings = {
+      close_on_focus_loss = true;
+      consider_preedit = true;
+      pop_to_root_on_close = true;
+      search_files_in_root = true;
+      favicon_service = "twenty";
+      font = {
+        normal = {
+          size = 12;
+          family = "Maple Nerd Font";
+        };
+      };
+    };
+    themes.rose-pine-moon = rawThemeData;
   };
 
 }
