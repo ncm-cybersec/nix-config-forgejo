fe117b61e088fb5b69a11f252980782562a4ee08
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-11 10:15:25 -0400

    fixed formatting

--- modules/user/utilities/rclone/default.nix
+++ modules/user/utilities/rclone/default.nix
@@ -9,33 +9,34 @@
 }:
 
 {
+  
   systemd.user.services.rclone-gdrive = {
-  Unit.Description = "Mount Google Drive";
-  Service = {
-    Environment = [
-      "PATH=/run/wrappers/bin:$PATH"
-    ];
-    ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: /home/nixadmin/mnt/gdrive --vfs-cache-mode full";
-    ExecStop = "/run/wrappers/bin/fusermount -u /home/nixadmin/mnt/gdrive";
-    Restart = "on-failure";
-    RestartSec = "10s";
+    Unit.Description = "Mount Google Drive";
+    Service = {
+      Environment = [
+        "PATH=/run/wrappers/bin:$PATH"
+      ];
+      ExecStart = "${pkgs.rclone}/bin/rclone mount gdrive: /home/nixadmin/mnt/gdrive --vfs-cache-mode full";
+      ExecStop = "/run/wrappers/bin/fusermount -u /home/nixadmin/mnt/gdrive";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+    Install.WantedBy = [ "default.target" ];
   };
-  Install.WantedBy = [ "default.target" ];
-};
 
-systemd.user.services.rclone-onedrive = {
-  Unit.Description = "Mount OneDrive";
-  Service = {
-    Environment = [
-      "PATH=/run/wrappers/bin:$PATH"
+  systemd.user.services.rclone-onedrive = {
+    Unit.Description = "Mount OneDrive";
+    Service = {
+      Environment = [
+        "PATH=/run/wrappers/bin:$PATH"
+      ];
+      ExecStart = "${pkgs.rclone}/bin/rclone mount onedrive: /home/nixadmin/mnt/onedrive --vfs-cache-mode full";
+      ExecStop = "/run/wrappers/bin/fusermount -u /home/nixadmin/mnt/onedrive";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+    Install.WantedBy = [
+      "default.target" 
     ];
-    ExecStart = "${pkgs.rclone}/bin/rclone mount onedrive: /home/nixadmin/mnt/onedrive --vfs-cache-mode full";
-    ExecStop = "/run/wrappers/bin/fusermount -u /home/nixadmin/mnt/onedrive";
-    Restart = "on-failure";
-    RestartSec = "10s";
-  };
-  Install.WantedBy = [
-    "default.target" 
-  ];
   };
 }
