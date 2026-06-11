5e6031f0fc3ce059fe31d45398cc00a18a577aff
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-11 10:18:10 -0400

    created module for proton pass ssh agent

---
+++ modules/user/utilities/protonpass/default.nix
@@ -0,0 +1,43 @@
+# ==========================================================================
+# Proton Pass CLI/SSH Manager Configuration
+# ==========================================================================
+
+{ 
+  pkgs, 
+  pkgsUnstable,
+  ... 
+}: 
+
+{
+  
+  # Enable Proton Pass Agent - Used for SSH key management 
+  services.proton-pass-agent = {
+    enable = true;
+    package = with pkgsUnstable; [
+      proton-pass-cli
+    ];
+    socket = "proton-pass-agent";
+    extraArgs = [
+      "--share-id"
+      "--vault-name"
+      "SshKeyVault"
+      "--refresh-interval"
+      "7200"
+      "--create-new-identities"
+      "SshKeyVault"
+    ];
+  };
+
+  # Proton Pass Agent Systemd Service - Unit File
+  systemd.user.services.proton-pass-ssh-agent = {
+    Unit = {
+      Description = "Proton Pass SSH Agent";
+    };
+    Service = {
+      ExecStart = "${pkgsUnstable.proton-pass-cli}/bin/pass-cli ssh-agent start --socket-path %h/.ssh/proton-pass-agent.sock";
+      Restart = "on-failure";
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+
+}
