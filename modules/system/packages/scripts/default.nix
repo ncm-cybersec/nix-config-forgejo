8d9676640da1a7475d857c355726c4fe4617c324
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-15 22:53:09 -0400

    declared zsh scripts system-wide

---
+++ modules/system/packages/scripts/default.nix
@@ -0,0 +1,21 @@
+# ==========================================================================
+# System Scripts
+# ==========================================================================
+
+{ 
+  pkgs,
+  self,
+  ... 
+}: 
+
+{
+  environment.systemPackages = [ 
+    
+    # ZSH script to help automate git stage and commit
+    (pkgs.writeShellScriptBin "git-stage-commit" (builtins.readFile "${self}/scripts/git-stage-commit.zsh"))
+    
+    # ZSH script to download the latest warp Appimage, update via gearlever cli and clean up
+    (pkgs.writeShellScriptBin "update-warp" (builtins.readFile "${self}/scripts/update-warp.sh"))
+
+  ];
+}
