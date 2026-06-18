0144e6873a363547ddb01a2506d148536fc9da2d
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-18 11:41:30 -0400

    fixed syntax

--- modules/system/packages/scripts/default.nix
+++ modules/system/packages/scripts/default.nix
@@ -15,7 +15,7 @@
     (pkgs.writeShellScriptBin "git-stage-commit" (builtins.readFile "${self}/scripts/git-stage-commit.zsh"))
     
     # ZSH script to download the latest warp Appimage, update via gearlever cli and clean up
-    (pkgs.writeShellScriptBin "update-warp" (builtins.readFile "${self}/scripts/update-warp.sh"))
+    (pkgs.writeShellScriptBin "update-warp" (builtins.readFile "${self}/scripts/update-warp.zsh"))
 
   ];
 }
