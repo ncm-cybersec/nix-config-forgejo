7c57d5102484ad8253f2234fca77d4f3db937006
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-19 09:53:45 -0400

    added netcatty script to system via writeShellScriptBin

--- modules/system/packages/scripts/default.nix
+++ modules/system/packages/scripts/default.nix
@@ -17,5 +17,8 @@
     # ZSH script to download the latest warp Appimage, update via gearlever cli and clean up
     (pkgs.writeShellScriptBin "update-warp" (builtins.readFile "${self}/scripts/update-warp.zsh"))
 
+    # ZSH script to download the latest Netcatty Appimage, update to latest version, update .desktop file, and clean up
+    (pkgs.writeShellScriptBin "update-netcatty" (builtins.readFile "${self}/scripts/update-netcatty.zsh"))
+
   ];
 }
