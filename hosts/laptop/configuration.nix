7056d6ac4db1c2c776bff76c6ff5679e9480d138
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-06-23 00:14:46 -0400

    added sops config and scripted nixos-upgrade so second host syncs with main, updates from forgjo (git) remote

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -8,6 +8,8 @@
 
 
 { 
+  config,
+  lib,
   pkgs,
   ... 
 }: 
@@ -55,19 +57,33 @@
     ];
   };
 
-  # Allow root to access the flake git repo (my nix-config folder lives in home directory, and is symlinked into /etc/nixos, so this is required for auto-upgrade)
-  systemd.services.nixos-upgrade = {
-    preStart = ''
-      git config --global --add safe.directory /home/nixpgadmin/nix-config
-    '';
+  # SOPS configuration for access to secrets.yaml, which is needed for access to the forgejo repository to complete the CI pipeline.
+  sops.defaultSopsFile = ../../secrets/secrets.yaml;
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt"; 
+
+  sops.secrets.forgejo_ssh_config = {
+    # Securely writes the secret directly to the root user's SSH config folder at boot.
+    path = "/root/.ssh/config";
   };
 
-  # Automatically install system updates daily at 11pm
+  # This forces the system to pull the latest changes from the forgejo repository before rebuilding to ensure that the additional host pulls changes from the central repo so that it's local configuration stays in sync.
+  # Without this, the additional host would not pull changes from the central repo and would become out of sync with the primary system, which would cause the CI pipeline to fail.
+  systemd.services.nixos-upgrade.serviceConfig.ExecStartPre = "${pkgs.git}/bin/git -C /etc/nixos pull";
+
+  # Auto-upgrade flake from my self-hosted forgejo repository
   system.autoUpgrade = {
     enable = true;
-    allowReboot = true;
-    dates = "23:00";
-    flake = "git+file:///home/nixpgadmin/nix-config";
+    
+    # "flake" instructs NixOS to look at a git repository rather than traditional channels
+    flake = "git+ssh://git@{ config.sops.secrets.forgejo_ssh_config.path; }/nas_forgejoadmin/nixos-config.git#nixlaptop";
+    
+    # Run intervals every day at midnight
+    dates = "00:00"; 
+    
+    # Options passed during the rebuild phase; auto-update nixpkgs dependencies.
+    flags = [
+      "--update-input" "nixpkgs"
+    ];
   };
 
   # Run garbage collection every Sunday at 3pm
