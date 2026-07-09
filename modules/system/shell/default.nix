c2bb01f1bf607a09c6878d0cfb5ffc544c2fe05e
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-07-08 22:57:31 -0400

    updated zsh aliases

--- modules/system/shell/default.nix
+++ modules/system/shell/default.nix
@@ -32,13 +32,19 @@
       gb = "git branch -m main";
       gi = "git init";
       gm = "git commit -m";
+      gsc = "./scripts/git-stage-commit.zsh";
+      gscym = "./scripts/git-stage-commit.zsh -y -m";
       gs = "git status";
       gsp = "git status -s -b";
       gpm = "git push -u origin main";
       ipas = "ip addr show";
       ll = "ls -la";
+      ncatup = "update-netcatty";
       nfu = "cd nix-config && nix flake update";
+      nrda = "sudo nixos-rebuild dry-activate";
       nrs = "sudo nixos-rebuild switch";
+      psyncup = "plasma-sync diff --update";
+      wup = "update-warp";
     };
     ohMyZsh = {
       enable = true;
