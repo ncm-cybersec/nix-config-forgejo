7c4b44c2fe29bf2f0a68eefd1f23f043ed48080b
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-07-11 22:56:32 -0400

    added polychromatic for razer devices

--- modules/system/hardware/default.nix
+++ modules/system/hardware/default.nix
@@ -14,6 +14,7 @@
     bluez-tools
     input-leap
     pavucontrol
+    polychromatic
     
   ];
 
@@ -79,6 +80,7 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      wireplumber.enable = true;
     };
   };
   
