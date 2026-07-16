623138f63cb47b477a3c2229ad965963bc4b86f9
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-07-15 22:21:44 -0400

    added aliases for adb to clear android cache

--- modules/system/shell/default.nix
+++ modules/system/shell/default.nix
@@ -36,6 +36,8 @@
     histSize = 10000;
    
     shellAliases = {
+      adblist = "adb shell pm list packages";
+      adbtrim = "adb shell pm trim-caches 128G";
       c = "clear";
       cdu = "cd ..";
       et = "eza --tree";
