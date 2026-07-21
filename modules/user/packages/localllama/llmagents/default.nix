a910d99e5162c242f8fb14dcf553e1d7243f32dd
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-07-21 16:55:22 -0400

    updated pkgs

--- modules/user/packages/localllama/llmagents/default.nix
+++ modules/user/packages/localllama/llmagents/default.nix
@@ -1,5 +1,5 @@
 # ==========================================================================
-# Home Manager Packages - Numtide/LLMAgents Flake Input
+# Local LLM Stack  - User Services
 # ==========================================================================
 
 { 
@@ -10,43 +10,26 @@
 
 {
   
-  # Packages from numtide/llm-agents.nix flake input
-  home.packages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
+  # Numtide/LLM-agents Packages
+  home.packages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [ 
     
-    # Assistants
-    hermes-agent
-    hermes-desktop
-    
-    # Coding Agents
-    oh-my-opencode
-    opencode
-    
-    # Memory
     gitnexus
     gno
-    memvid-cli
-
-    # Utilities
-    apm
-    mcporter
-    openskills
-    skills
-    skills-installer
-    
-    # Workflow
-    agent-deck
     herdr
-      
+    kilocode-cli
+    letta-code
+    memvid-cli
+    nanocoder
+    oh-my-opencode
+    skills
+    
   ];
-
-    # ag-cli is antigravity-cli from llm-agents.nix; wrapper derivation to rename binary to avoid collision with IDE version from nixpkgs - reference 
-
-    #(pkgs.symlinkJoin {
-    #  name = "ag-cli";
-    #  paths = [ antigravity ];
-    #  postBuild = '' 
-    #    mv "$out/bin/antigravity" "$out/bin/ag-cli"
-    #  '';
-    #})
-
+  
+  # Enable Opencode using pkg from llmagents flakes
+  programs = {
+    opencode = {
+      enable = true;
+      package = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode;
+    };
+  };
 }
