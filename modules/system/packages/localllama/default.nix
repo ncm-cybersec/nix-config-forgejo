8b13e1ffe4cdbb080e08260b0feb76514ea5b0f4
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-07-22 18:11:37 -0400

    added models to ollama

--- modules/system/packages/localllama/default.nix
+++ modules/system/packages/localllama/default.nix
@@ -22,23 +22,26 @@
       host = "127.0.0.1";
       port = 11434;
       
+      # Dynamically load models based on hostname
       syncModels = true;
       loadModels = lib.mkMerge [
+        
+        # Desktop
         (lib.mkIf (hostName == "nixadmin") [
-          #"gemma4:12b"
-          #"ornith:9b"
+          "gemma4:12b"
           "qwen2.5-coder:14b"
-          #"qwen3.5:9b"
+          "qwen3.5:9b"
         ])
         
+        # Laptop
         (lib.mkIf (hostName == "nixpgadmin") [
-          #"qwen2.5-coder:7b"
+          "qwen2.5-coder:7b"
           "qwen3:4b"
-          #"qwen3.5:4b"
+          "qwen3.5:4b"
         ])
       ];
     
-      # Enable Flash Attention globally for the daemon
+      # Enable Flash Attention
       environmentVariables = {
         OLLAMA_FLASH_ATTENTION = "1";
       };
