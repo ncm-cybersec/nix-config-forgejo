0306112133bc90b1c122f4f51a8fc598af7acc3c
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-07-22 22:42:50 -0400

    declared opencode.json

--- modules/user/packages/localllama/llmagents/default.nix
+++ modules/user/packages/localllama/llmagents/default.nix
@@ -30,6 +30,34 @@
     opencode = {
       enable = true;
       package = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode;
+      
+      # Opencode.json
+      settings = {
+        provider = {
+          ollama = {
+            npm = "@ai-sdk/openai-compatible";
+            name = "Ollama";
+            options = {
+              baseUrl = "http://127.0.0.1:11434";
+            };
+            models = {
+              "qwen2.5-coder:14b" = {
+                name = "Qwen2.5";
+              };
+              "gemma4:12b" = {
+                name = "Gemma4";
+              };
+              "qwen3.5:9b" = {
+                name = "Qwen3.5";
+              };
+            };
+          };
+        };
+        plugin = [
+          "opencode-mem"
+          "@warp-dot-dev/opencode-warp"
+        ];
+      };
     };
   };
 }
