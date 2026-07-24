31973387dca77178ccd720017c8a085e138149c8
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-07-24 19:32:48 -0400

    deduplicated rclone modules, both hosts now share the same

---
+++ modules/user/storage/rclone/default.nix
@@ -0,0 +1,70 @@
+# ==========================================================================
+# Home Manager Rclone Conf - Desktop & Laptop
+# ==========================================================================
+
+{ 
+  config,
+  pkgs,
+  ... 
+}:
+
+{
+
+  home.packages = with pkgs; [
+    rclone
+    remote-exec
+    rsync
+  ];
+  
+  sops.secrets = {
+    rclone-gd = {
+      neededForUsers = false;
+    };
+    
+    rclone-od = {
+      neededForUsers = false;
+    };
+  };
+  
+  # Mount GoogleDrive remote as systemd service
+  systemd.user.services.rclone-gdrive = {
+    Unit = {
+      Description = "Mount Google Drive";
+      After = [ "network-online.target" "sops-nix.service" ];
+      Wants = [ "network-online.target" "sops-nix.service" ];
+    };
+    
+    Service = {
+      EnvironmentFile = [ "%t/sops-nix/secrets/rclone-gd" ];
+      Environment = [ "PATH=/run/wrappers/bin:$PATH" ];
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${config.home.homeDirectory}/mnt/GoogleDrive";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount googledrive: ${config.home.homeDirectory}/mnt/GoogleDrive --vfs-cache-mode full --poll-interval 15s";
+      ExecStop = "/run/wrappers/bin/fusermount -u ${config.home.homeDirectory}/mnt/GoogleDrive";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+    
+    Install.WantedBy = [ "default.target" ];
+  };
+
+  # Mount OneDrive remote as systemd service
+  systemd.user.services.rclone-onedrive = {
+    Unit = {
+      Description = "Mount OneDrive";
+      After = [ "network-online.target" "sops-nix.service" ];
+      Wants = [ "network-online.target" "sops-nix.service" ];
+    };
+    
+    Service = {
+      Environment = [ "PATH=/run/wrappers/bin:$PATH" ];
+      EnvironmentFile = [ "%t/sops-nix/secrets/rclone-od" ];
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${config.home.homeDirectory}/mnt/OneDrive";
+      ExecStart = "${pkgs.rclone}/bin/rclone mount onedrive: ${config.home.homeDirectory}/mnt/OneDrive --vfs-cache-mode full";
+      ExecStop = "/run/wrappers/bin/fusermount -u ${config.home.homeDirectory}/mnt/OneDrive";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+    
+    Install.WantedBy = [ "default.target" ];
+  };
+}
