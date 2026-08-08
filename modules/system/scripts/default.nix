ef3b5c5a0aeac12354209ba4474e82f6fa59816f
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-08-07 21:01:08 -0400

    created a script to cycle through appimage updates & defined systemd service/timer to run on a daily timer

--- modules/system/scripts/default.nix
+++ modules/system/scripts/default.nix
@@ -11,18 +11,27 @@
 let
   
   # Define scripts as local Nix variables
+  git-stage-commit = pkgs.writeShellScriptBin "git-stage-commit" (builtins.readFile "${self}/scripts/git-stage-commit.zsh");
+
+  nixos-upgrade-failed = pkgs.writeShellScriptBin "nixos-upgrade-failed" (builtins.readFile "${self}/scripts/nixos-upgrade-failed.zsh");
+  
   plasma-sync = pkgs.writers.writePython3Bin "plasma-sync" { 
     libraries = [ ]; 
     flakeIgnore = [ "E265" "E302" "E305" "E501" "F401" "W291" "W292" "W293" ]; 
   } (builtins.readFile "${self}/scripts/plasma-sync.py");
-
-  git-stage-commit = pkgs.writeShellScriptBin "git-stage-commit" (builtins.readFile "${self}/scripts/git-stage-commit.zsh");
   
-  nixos-upgrade-failed = pkgs.writeShellScriptBin "nixos-upgrade-failed" (builtins.readFile "${self}/scripts/nixos-upgrade-failed.zsh");
-  
-  update-netcatty = pkgs.writeShellScriptBin "update-netcatty" (builtins.readFile "${self}/scripts/update-netcatty.zsh");
-
-  update-warp = pkgs.writeShellScriptBin "update-warp" (builtins.readFile "${self}/scripts/update-warp.zsh");
+  update-appimages = (pkgs.writeShellScriptBin "update-appimages" ''
+    echo "Starting automated daily AppImage updates..."
+    update-copilot
+    update-letta
+    update-massCode
+    update-msty_nexus
+    update-mstystudio
+    update-netcatty
+    update-openwarp
+    update-warp
+    echo "AppImage updates completed."
+  '');
 
 in
 
@@ -30,11 +39,10 @@ in
   
   # Add scripts to systemPackages for manual usage via terminal
   environment.systemPackages = [ 
-    plasma-sync
     git-stage-commit
     nixos-upgrade-failed
-    update-netcatty
-    update-warp
+    plasma-sync
+    update-appimages
   ];
   
   # Systemd service to notify user of failed NixOS upgrade
@@ -43,5 +51,30 @@ in
     script = "${nixos-upgrade-failed}/bin/nixos-upgrade-failed %i";
     path = with pkgs; [ systemd gnused sudo libnotify ];
   };
-
+  
+  systemd.user.services."update-appimages" = {
+    description = "Automated background update script for AppImages";
+    path = [ pkgs.coreutils pkgs.curl pkgs.gnugrep pkgs.gnused pkgs.gawk pkgs.jq ];
+    
+    script = ''
+      ${update-appimages}/bin/update-appimages
+    '';
+    
+    serviceConfig = {
+      Type = "oneshot";
+      Nice = 19;
+      CPUSchedulingPolicy = "idle";
+    };
+  };
+  
+  systemd.user.timers."update-appimages" = {
+    description = "Timer for daily AppImage updates";
+    wantedBy = [ "timers.target" ];
+    
+    timerConfig = {
+      OnCalendar = "*-*-* 02:00:00";
+      Persistent = true;
+      RandomizedDelaySec = "15m";
+    };
+  };
 }
