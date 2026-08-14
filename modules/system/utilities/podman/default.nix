912e1936f7bad711eead6bb1494fdd04fc19cb74
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-08-13 21:32:12 -0400

    chore: updated virtualisation.podman options

--- modules/system/utilities/podman/default.nix
+++ modules/system/utilities/podman/default.nix
@@ -20,23 +20,17 @@
 
   # Podman virtualisation
   virtualisation = {
-    containers.enable = true;
     podman = {
       enable = true;
       dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
+      defaultNetwork.settings = {
+        dns_enabled = true;
+      };
+      
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
     };
   };
-
-  # Podman container registries
-  virtualisation.containers.registries.search = [
-    "docker.io"     # Docker Container Registry
-    "ghcr.io"       # Github Container Registry
-    "lscr.io"       # Linux Server Container Registry
-    "quay.io"       # Quay Container Registry
-  ];
-
-  # Pass NVIDIA GPU to containers
-  hardware.nvidia-container-toolkit.enable = true;
-
 }
