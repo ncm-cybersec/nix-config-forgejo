3621fdf7405549498b952e083ee884a45c9c0ae9
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-08-13 21:26:26 -0400

    ops: added aliases for update-appimages & nix-rebuild scripts

--- modules/system/shell/default.nix
+++ modules/system/shell/default.nix
@@ -15,48 +15,48 @@
   
   environment = {
     
-    # System shells
     shells =  with pkgs; [
       bashInteractive
       zsh
     ];
     
-    # Paths
     pathsToLink = [
       "/share/zsh"
     ];
   };
 
   # Enable ZSH w/ ohMyZSH (further config at user level)
-  programs.zsh = {
-    enable = true;
-    enableGlobalCompInit = config.programs.zsh.enableCompletion;
-    enableLsColors = true;
-    histSize = 10000;
+  programs = {
+    zsh = {
+      enable = true;
+      enableGlobalCompInit = config.programs.zsh.enableCompletion;
+      enableLsColors = true;
+      histSize = 10000;
    
-    shellAliases = {
-      adblist = "adb shell pm list packages";
-      adbtrim = "adb shell pm trim-caches 128G";
-      c = "clear";
-      cdu = "cd ..";
-      et = "eza --tree";
-      ga = "git add .";
-      gb = "git branch -m main";
-      gi = "git init";
-      gm = "git commit -m";
-      gsc = "./scripts/git-stage-commit.zsh";
-      gscym = "./scripts/git-stage-commit.zsh -y -m";
-      gs = "git status";
-      gsp = "git status -s -b";
-      gpm = "git push -u origin main";
-      ipas = "ip addr show";
-      ll = "ls -la";
-      ncatup = "update-netcatty";
-      nfu = "cd nix-config && nix flake update";
-      nrda = "sudo nixos-rebuild dry-activate";
-      nrs = "sudo nixos-rebuild switch";
-      psyncup = "plasma-sync diff --update";
-      wup = "update-warp";
+      shellAliases = {
+        adblist = "adb shell pm list packages";
+        adbtrim = "adb shell pm trim-caches 128G";
+        appup = "update-appimages";
+        c = "clear";
+        cdu = "cd ..";
+        et = "eza --tree";
+        ga = "git add .";
+        gb = "git branch -m main";
+        gi = "git init";
+        gm = "git commit -m";
+        gsc = "./scripts/git-stage-commit.zsh";
+        gscym = "./scripts/git-stage-commit.zsh -y -m";
+        gs = "git status";
+        gsp = "git status -s -b";
+        gpm = "git push -u origin main";
+        ipas = "ip addr show";
+        ll = "ls -la";
+        nfu = "cd nix-config && nix flake update";
+        nrb = "sudo nix-rebuild";
+        nrda = "sudo nixos-rebuild dry-activate";
+        nrs = "sudo nixos-rebuild switch";
+        psyncup = "plasma-sync diff --update";
+      };
     };
   };
 }
