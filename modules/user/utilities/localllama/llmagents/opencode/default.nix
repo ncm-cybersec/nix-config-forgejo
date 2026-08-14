cbe0549eb86795ac13de17405c839a77618e31c3
nixadmin <ncm-cybersec@users.noreply.github.com>
2026-08-13 23:06:46 -0400

    refactor: created opencode subdirectory to organize llmagents module

---
+++ modules/user/utilities/localllama/llmagents/opencode/default.nix
@@ -0,0 +1,61 @@
+# ==========================================================================
+# Local LLM Stack  - OpenCode
+# ==========================================================================
+
+{ 
+  inputs,
+  pkgs,
+  ... 
+}:
+
+{
+
+  # Enable Opencode using pkg from llmagents flakes
+  programs = {
+    opencode = {
+      enable = true;
+      package = inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.opencode;
+      
+      # opencode.json
+      settings = {
+        shell = "/bin/zsh";
+        model = "qwen3.5:9b";
+        default_agent = "plan";
+        autoupdate = false;
+        lsp = true;
+        formatter = true;
+        provider = {
+          ollama = {
+            models = {
+              "hf.co/RavichandranJ/Dolphin3-Cyber-8B-GGUF:Q8_0" = {
+                name = "Dolphin3-Cyber";
+              };
+              "gemma4:12b" = {
+                name = "Gemma4";
+              };
+              "ornith:9b" = {
+                name = "Ornith";
+              };
+              "qwen2.5-coder:14b" = {
+                name = "Qwen2.5";
+              };
+              "qwen3.5:9b" = {
+                name = "Qwen3.5";
+              };
+            };
+            name = "Ollama";
+            npm = "@ai-sdk/openai-compatible";
+            options = {
+              baseURL = "http://localhost:11434/v1";
+              extraBody = {
+                options = {
+                  num_ctx = 32768;
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
